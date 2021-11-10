2cc1cd5b4e93ffb7f36d136c84a5493bb9dd1015
Owner <deema@danon.tzafonet.org.il>
2021-11-10 08:40:40 +0200

    after designing all activities all activities graphics components (view) are declaered

--- app/src/main/java/hajar/ashwah/hajartaskmanager/SplashScreenActivity.java
+++ app/src/main/java/hajar/ashwah/hajartaskmanager/SplashScreenActivity.java
@@ -1,7 +1,9 @@
 package hajar.ashwah.hajartaskmanager;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -10,5 +12,23 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+         //Thread : 1
+        Thread th=new Thread(){
+            //Thread : 2
+            @Override
+            public void run() {
+                // المقطع الذي سيعمل بالتزامن مع مقاطع اخرى
+                //Thread : 3
+                int ms=3*1000;//milliseconds
+                try {
+                    sleep(ms);
+                   startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+
     }
 }
